c6c0aac2d8bf91f72b35b20da2a334ac58e9d419
keppeler <keppeler@iastate.edu>
2019-03-21 14:32:04 -0500

    created new android studio project, and set up HTTPRequestHandler Singleton class for the app

---
+++ Frontend/app/src/main/java/chat/safe/safechat/MainActivity.java
@@ -0,0 +1,44 @@
+package chat.safe.safechat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+    }
+
+    public void sendRequestButton(View v){
+        String url = "http://www.google.com";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //Display the first 500 characters of the response string
+                textView.setText("Response is: " + response.substring(0, 500));
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+
+        RequestHandler.getInstance(this).addToRequestQueue(stringRequest);
+    }
+}
